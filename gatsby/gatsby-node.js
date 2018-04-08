14d30d128cd2bf097983dff050b37402272345e3
Grayson Hicks <graysonhicks@gmail.com>
2018-04-08 17:26:25 -0400

    basic github graphql api query

--- gatsby/gatsby-node.js
+++ gatsby/gatsby-node.js
@@ -4,4 +4,84 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
- // You can delete this file if you're not using it
+// You can delete this file if you're not using it
+const crypto = require('crypto')
+const github = require('octonode')
+
+const client = github.client('c52cc37d818c7f7a6c8025b155ac3c756374cead')
+exports.sourceNodes = async ({ boundActionCreators }) => {
+  const { createNode } = boundActionCreators
+
+  const buildRepoNodes = repos => {
+    repos.forEach(repo => {
+      const repoNode = {
+        id: repo.id.toString(),
+        parent: null,
+        children: [],
+        internal: {
+          type: `GitHubRepo`,
+          contentDigest: crypto
+            .createHash(`md5`)
+            .update(JSON.stringify(repo))
+            .digest(`hex`),
+        },
+        name: repo.name,
+        description: repo.description,
+        url: repo.html_url,
+        updated: repo.updated_at,
+        created: repo.created_at,
+      }
+      createNode(repoNode)
+    })
+  }
+
+  const buildUserNode = user => {
+    const userNode = {
+      id: user.id.toString(),
+      parent: null,
+      children: [],
+      internal: {
+        type: `GitHubUser`,
+        contentDigest: crypto
+          .createHash(`md5`)
+          .update(JSON.stringify(user))
+          .digest(`hex`),
+      },
+      name: user.name,
+      avatar_url: user.avatar_url,
+      url: user.html_url,
+      followers: user.followers,
+      followers_url: user.followers_url,
+      following_url: user.following_url,
+      following: user.following,
+      public_repos: user.public_repos,
+      created_at: user.created_at,
+      updated_at: user.updated_at,
+    }
+    createNode(userNode)
+  }
+  return new Promise((resolve, reject) => {
+    const ghme = client
+      .me()
+      .repos({ per_page: 100, type: 'owner' }, function(err, body, headers) {
+        if (err) {
+          console.log('\nError')
+          console.log(err)
+          reject()
+        } else {
+          const data = {}
+          buildRepoNodes(body)
+          client.get('users/graysonhicks', (err, status, body, headers) => {
+            if (err) {
+              console.log('\nError')
+              console.log(err)
+              reject()
+            } else {
+              buildUserNode(body)
+              resolve()
+            }
+          })
+        }
+      })
+  })
+}
