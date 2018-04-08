b2281b842aac639a5423c0aa1b5662eb8037e469
Grayson Hicks <graysonhicks@gmail.com>
2018-04-08 17:31:53 -0400

    double github repo working! ! gaa gp

--- gatsby/src/pages/github.js
+++ gatsby/src/pages/github.js
@@ -2,5 +2,48 @@ import React from 'react'
 
 import GitHub from '../components/GitHub'
 
-const GitHubPage = () => <GitHub />
+const GitHubPage = ({ data: { allGitHubRepo, allGitHubUser } }) => {
+  return (
+    <GitHub
+      repos={allGitHubRepo.edges}
+      user={allGitHubUser.edges}
+      totalRepos={allGitHubRepo.totalCount}
+    />
+  )
+}
 export default GitHubPage
+
+export const GitHubQuery = graphql`
+  query GitHubQuery {
+    allGitHubRepo {
+      totalCount
+      edges {
+        node {
+          id
+          name
+          description
+          url
+          updated
+          created
+        }
+      }
+    }
+    allGitHubUser {
+      edges {
+        node {
+          id
+          name
+          avatar_url
+          url
+          followers
+          followers_url
+          following
+          followers_url
+          public_repos
+          created_at
+          updated_at
+        }
+      }
+    }
+  }
+`
