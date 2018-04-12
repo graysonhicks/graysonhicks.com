557e4e0ff635ec45f3c8fd8df236db6811007b7b
Grayson Hicks <graysonhicks@gmail.com>
2018-04-12 11:51:58 -0400

    unnested favicons, helmet cant have nested

--- gatsby/src/components/SiteMetaDataHelmet/index.js
+++ gatsby/src/components/SiteMetaDataHelmet/index.js
@@ -2,8 +2,23 @@ import React from 'react'
 import { Helmet } from 'react-helmet'
 
 import AppContext from '../../context'
-import FavIcons from './FavIcons'
 import cardPic from './carolina.jpg'
+
+import Apple57 from './icons/apple-icon-57x57.png'
+import Apple60 from './icons/apple-icon-60x60.png'
+import Apple72 from './icons/apple-icon-72x72.png'
+import Apple76 from './icons/apple-icon-76x76.png'
+import AppleTouch114 from './icons/apple-icon-114x114.png'
+import AppleTouch120 from './icons/apple-icon-120x120.png'
+import AppleTouch144 from './icons/apple-icon-144x144.png'
+import AppleTouch152 from './icons/apple-icon-152x152.png'
+import AppleTouch180 from './icons/apple-icon-180x180.png'
+import Android192 from './icons/android-icon-192x192.png'
+import FavIcon32 from './icons/favicon-32x32.png'
+import FavIcon96 from './icons/favicon-96x96.png'
+import FavIcon16 from './icons/favicon-16x16.png'
+import MsIcon144 from './icons/ms-icon-144x144.png'
+import manifest from './icons/manifest.json'
 
 const SiteMetaDataHelmet = () => (
   <AppContext.Consumer>
@@ -58,7 +73,24 @@ const SiteMetaDataHelmet = () => (
           integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
           crossorigin="anonymous"
         />
-        <FavIcons />
+        <link rel="apple-touch-icon" sizes="57x57" href={Apple57} />
+        <link rel="apple-touch-icon" sizes="60x60" href={Apple60} />
+        <link rel="apple-touch-icon" sizes="72x72" href={Apple72} />
+        <link rel="apple-touch-icon" sizes="76x76" href={Apple76} />
+        <link rel="apple-touch-icon" sizes="114x114" href={AppleTouch114} />
+        <link rel="apple-touch-icon" sizes="120x120" href={AppleTouch120} />
+        <link rel="apple-touch-icon" sizes="144x144" href={AppleTouch144} />
+        <link rel="apple-touch-icon" sizes="152x152" href={AppleTouch152} />
+        <link rel="apple-touch-icon" sizes="180x180" href={AppleTouch180} />
+        <link rel="icon" type="image/png" sizes="192x192" href={Android192} />
+        <link rel="icon" type="image/png" sizes="32x32" href={FavIcon32} />
+        <link rel="icon" type="image/png" sizes="96x96" href={FavIcon96} />
+        <link rel="icon" type="image/png" sizes="16x16" href={FavIcon16} />
+        <link rel="manifest" href="./manifest.json" />
+        <meta name="msapplication-config" content="./icons/browserconfig.xml" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="msapplication-TileImage" content={MsIcon144} />
+        <meta name="theme-color" content="#ffffff" />
         <title>Grayson Hicks | Software Developer</title>
       </Helmet>
     )}
