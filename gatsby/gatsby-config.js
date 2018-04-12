e58338d5104a2fb9081b2801aad7f4e4af94a510
Grayson Hicks <graysonhicks@gmail.com>
2018-04-12 12:09:06 -0400

    offline plugin

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-react-next',
     'gatsby-plugin-styled-components',
+    'gatsby-plugin-offline',
     {
       resolve: `gatsby-plugin-sitemap`,
     },
